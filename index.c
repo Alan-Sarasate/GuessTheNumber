3fd612216008fd638ec5d9cad403c0d60c1c2ce4
Alan Sarasate <alansarasatecc@gmail.com>
2023-09-18 00:16:07 -0300

    feat: Modularização da entrada de inteiros e adição de nova(s) biblioteca(s).

--- index.c
+++ index.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 void jogoFacil();
@@ -9,10 +10,9 @@ int main(){
     int option = 1, numero;
 
     do{
-        printf("\t\tGUESS THE NUMBER\n\n|  --> Selecione a dificuldade do jogo:  |\n\n|1 - Easy (1-50, sem limite de vidas)\n|2 - Medium (1-50, 10 vidas)\n|3 - Hard (1-100, 10 vidas )\n|4 - Very Hard (1-100, 5 vidas)\n|5 - Personalizado (Escolha o range e a qauntidade de palpites)\n|0 - Sair\n\n--> ");
+        printf("\t\tGUESS THE NUMBER\n\n|  --> Selecione a dificuldade do jogo:  |\n\n|1 - Easy (1-50, sem limite de vidas)\n|2 - Medium (1-50, 10 vidas)\n|3 - Hard (1-100, 10 vidas )\n|4 - Very Hard (1-100, 5 vidas)\n|5 - Personalizado (Escolha o range e a quantidade de palpites)\n|0 - Sair\n\n--> ");
         scanf("%d", &option);
-        switch (option)
-        {
+        switch (option){
         case 1:
             printf("\t\tJOGO FACIL\n\n");
             numero = rand() % 50;
@@ -34,11 +34,17 @@ int main(){
     return 0;
 }
 
+int retornarInteiro(){
+    int valor;
+    printf("Digite um inteiro: ");
+    scanf("%d", &valor);
+    return valor;
+}
+
 void jogoFacil(int numero){
     int ultimoPalpite, palpite, palpites = 0;
     do{
-        printf("Digite um inteiro: ");
-        scanf("%d", &palpite);
+        palpite = retornarInteiro();
         printf("\n");
         palpites++;
         ultimoPalpite = palpite;
@@ -54,8 +60,7 @@ void jogoFacil(int numero){
 void jogoMedio(int numero){
     int ultimoPalpite, palpite, palpites = 0, vidas = 10;
     do{
-        printf("Digite um inteiro: ");
-        scanf("%d", &palpite);
+        palpite = retornarInteiro();
         printf("\n");
         palpites++;
         ultimoPalpite = palpite;
